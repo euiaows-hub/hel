4f830682694fdde802ac60630eb39cbdfc42855b
euiaows-hub <euiaows@gmail.com>
2025-09-09 23:04:58 +0500

    Update ss.js

--- ss.js
+++ ss.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 
+// Главная страница
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+// Тех. проверка (на всякий)
+app.get("/healthz", (req, res) => {
+  res.status(200).send("ok");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Server running on port ${PORT}');
+  console.log('Listening on ${PORT}');
 });
